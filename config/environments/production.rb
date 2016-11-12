aea47d9a19cd2c7552564d2152a43490fef5879b
BlueAeroplane <sablebrownglibert@hotmail.com>
2016-11-12 16:56:50 +0000

    init2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://sitepoint-actioncable.herokuapp.com',
-                                                 'http://sitepoint-actioncable.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://yakchat.herokuapp.com',
+                                                 'http://yakchat.herokuapp.com']
 
-  config.action_cable.url = "wss://sitepoint-actioncable.herokuapp.com/cable"
+  config.action_cable.url = "wss://yakchat.herokuapp.com/cable"
 end
